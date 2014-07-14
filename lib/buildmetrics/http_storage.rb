a497cec232431a16c291bb3528c1501f7071362d
Solomon White <rubysolo@gmail.com>
2014-07-14 15:36:13 -0600

    add support for custom headers

--- lib/buildmetrics/http_storage.rb
+++ lib/buildmetrics/http_storage.rb
@@ -4,14 +4,19 @@ module Buildmetrics
   class HttpStorage
     attr_reader :uri
 
-    def initialize(url)
+    def initialize(url, headers={})
+      @headers = headers
       @uri = URI.parse(url)
     end
 
     def store(build)
-      req = Net::HTTP::Post.new(@uri.path, {'Content-Type' =>'application/json'})
+      req = Net::HTTP::Post.new(@uri.path, headers)
       req.body = build.to_json
       Net::HTTP.new(@uri.host, @uri.port).start { |http| http.request(req) }
+    end
+
+    def headers
+      @headers.update('Content-Type' => 'application/json')
     end
   end
 end
